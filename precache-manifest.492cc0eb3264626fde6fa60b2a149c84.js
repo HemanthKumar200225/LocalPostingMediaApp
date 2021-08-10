9e3a6dac6689db6152b43f8e8e06706eb2f5c321
HemanthKumar25116 <hemanthkumar200225@gmail.com>
2021-08-10 14:24:44 +0530

    Updates

---
+++ precache-manifest.492cc0eb3264626fde6fa60b2a149c84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d37ee9062bf7d110c1f2a5194513b2e",
+    "url": "/LocalPostingMediaApp/index.html"
+  },
+  {
+    "revision": "6290ff1b492104959533",
+    "url": "/LocalPostingMediaApp/static/css/main.b0211d57.chunk.css"
+  },
+  {
+    "revision": "4a18124ab7350b413c52",
+    "url": "/LocalPostingMediaApp/static/js/2.b627b45f.chunk.js"
+  },
+  {
+    "revision": "3f598473a645111fe9a1b1e43cd3e4b8",
+    "url": "/LocalPostingMediaApp/static/js/2.b627b45f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6290ff1b492104959533",
+    "url": "/LocalPostingMediaApp/static/js/main.b64e6ba4.chunk.js"
+  },
+  {
+    "revision": "84312e521b00571ed674",
+    "url": "/LocalPostingMediaApp/static/js/runtime-main.a8c5ebf6.js"
+  }
+]);
